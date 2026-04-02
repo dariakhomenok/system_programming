6b74654cc7914601bfce88b22f4cf77d1afe9b94
Darya <daria.khomenok12317@gmail.com>
2026-04-02 10:31:11 +0700

    Коммит 3: Задание 2 Скольким часам на Земле равен день на Меркурии

---
+++ glava4/task2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+ float mercury_day_in_earth_days();
+ int hours_in_an_earth_day();
+
+ int main()
+ {
+    float lenght_of_day = mercury_day_in_earth_days();
+    int hours = hours_in_an_earth_day();
+    float day = lenght_of_day * hours;
+    printf("День на Меркурии равен %f часам\n", day);
+    return 0;
+ }
+
+ float mercury_day_in_earth_days()
+ {
+    return 58.65;
+ }
+
+ int hours_in_an_earth_day()
+ {
+    return 24;
+ }
+
+// [root@localhost book]# ./a.out
+// День на Меркурии равен 1407.600098 часам
